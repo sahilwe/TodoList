074285a24b7eac01fdfc8eac7fa03e480e61611f
Sahil Verma <sahilverma92942@gmail.com>
2022-06-27 16:57:21 -0930

    procfile added,set port

--- app.js
+++ app.js
@@ -209,7 +209,10 @@ app.get("/:paramName", function(req,res){
   // if ( )
 
 })
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+};
+app.listen(port, function() {
+  console.log("Server started Succesfully");
 });
